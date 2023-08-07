c91be848f10144897188fb67c714f841aeb0d9a7
swapnil patil <swapnil.patil@itariumtech.com>
2023-08-07 18:42:10 +0530

    Create Notification.java

---
+++ src/main/java/com/cbs/middleware/domain/Notification.java
@@ -0,0 +1,182 @@
+package com.cbs.middleware.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+import javax.persistence.*;
+
+/**
+ * A Notification.
+ */
+@Entity
+@Table(name = "notification")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Notification implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "is_read")
+    private Boolean isRead;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "recipient")
+    private String recipient;
+
+    @Column(name = "sender")
+    private String sender;
+
+    @Column(name = "type")
+    private String type;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Notification id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Notification title(String title) {
+        this.setTitle(title);
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public Notification content(String content) {
+        this.setContent(content);
+        return this;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getIsRead() {
+        return this.isRead;
+    }
+
+    public Notification isRead(Boolean isRead) {
+        this.setIsRead(isRead);
+        return this;
+    }
+
+    public void setIsRead(Boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public Instant getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public Notification createdAt(Instant createdAt) {
+        this.setCreatedAt(createdAt);
+        return this;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getRecipient() {
+        return this.recipient;
+    }
+
+    public Notification recipient(String recipient) {
+        this.setRecipient(recipient);
+        return this;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSender() {
+        return this.sender;
+    }
+
+    public Notification sender(String sender) {
+        this.setSender(sender);
+        return this;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public Notification type(String type) {
+        this.setType(type);
+        return this;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Notification)) {
+            return false;
+        }
+        return id != null && id.equals(((Notification) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Notification{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", content='" + getContent() + "'" +
+            ", isRead='" + getIsRead() + "'" +
+            ", createdAt='" + getCreatedAt() + "'" +
+            ", recipient='" + getRecipient() + "'" +
+            ", sender='" + getSender() + "'" +
+            ", type='" + getType() + "'" +
+            "}";
+    }
+}
